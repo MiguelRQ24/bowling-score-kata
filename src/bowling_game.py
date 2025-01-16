9f9944c8f3f27f8642e944bbc06d201aa7ff1abd
MiguelRQ24 <miguelrodriguezquintas@gmail.com>
2025-01-16 16:05:35 +0100

    feat(bowling_game.py): Añadir funcion __separte_in_frames():

--- src/bowling_game.py
+++ src/bowling_game.py
@@ -9,5 +9,21 @@ class BowlingCard:
     # Frame el conjunto de 2 tiradas 
     
     def total_score(self):
+
+        self.frames = self.__separte_in_frames()
+        print(self.frames)
         self.total = 0
+
         return self.total
+    
+    def __separte_in_frames(self):
+        rolls = self.rolls
+        frames = []
+        for position in range(10):
+            if position == 9:
+                frames.append(list(rolls))
+            elif rolls[0] != "X":
+                frames.append([rolls.pop(0), rolls.pop(0)])
+            else:
+                frames.append([rolls.pop(0)])
+        return frames
