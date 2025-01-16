46961fc33b294bfe7b1e77e54f0cbf88a323db8c
MiguelRQ24 <miguelrodriguezquintas@gmail.com>
2025-01-16 16:01:27 +0100

    feat(bowling_game.py): Añadir la funcion inicializadora.

---
+++ src/bowling_game.py
@@ -0,0 +1,11 @@
+
+
+class BowlingCard:
+    # una clase simpre tine que tener un tipo de dato oculto
+
+    def __init__(self, rolls):
+        self.rolls = list(rolls) # propiedad de instancia, encapsular
+    # roll tirada 
+    # Frame el conjunto de 2 tiradas 
+    
+    
